5fc27308461571819657587901e768be4e190fc3
Daghsny <61598628+Daghsny@users.noreply.github.com>
2026-05-07 11:05:47 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -156,7 +156,7 @@ const coursesData = [
         type: "TD",
         dateAdded: "2026-05-07",
         pdfUrl: "https://daghsny.github.io/sti/assets/TP/Excercises Php.pdf",
-        thumbnail: "https://static.vecteezy.com/ti/photos-gratuite/p1/3528838-petit-panier-avec-ordinateur-portable-pour-achat-en-ligne-concept-gratuit-photo.jpg"
+        thumbnail: "https://fieldnotesbrand.imgix.net/images/FN-18-Calendar-2026-Pages.jpg?ixlib=php-4.1.0&s=37f07777f7f3f0b07cc3f960b62df9d3"
     },
  // ******** PROJECTS  ********  
     {
